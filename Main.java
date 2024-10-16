5d6b0f5c1e9fa10d4e1a7f030d40021077982376
Denis-pool <kriv.denkk@gmail.com>
2024-10-16 21:55:34 +0300

    Инициализация проекта
     Добавил первый файл в репозиторий

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main (String[] args) {
+
+        // Просуммировать числа от 1 до 5
+
+        int sum = 1 + 2;
+        int sum1 = sum + 3;
+        int sum2 = sum1 + 4;
+        int result = sum2 + 5;
+        System.out.println(result);
+    }
+}
