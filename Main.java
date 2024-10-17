a7ec91583e4db65a4cc314351bd9dd88f2a59a6d
Denis-pool <kriv.denkk@gmail.com>
2024-10-17 12:22:20 +0300

    Изменил текс через GitHub

--- Main.java
+++ Main.java
@@ -4,10 +4,14 @@ public class Main {
 
         // Просуммировать числа от 1 до 5
 
-        int sum = 1 + 2;
-        int sum1 = sum + 3;
-        int sum2 = sum1 + 4;
-        int result = sum2 + 5;
-        System.out.println(result);
+      //  int sum = 1 + 2;
+      //  int sum1 = sum + 3;
+      //  int sum2 = sum1 + 4;
+      // int result = sum2 + 5;
+      //  System.out.println(result);
+
+
+        System.out.print("Написано однажды, ");
+        System.out.println("работает везде!");
     }
 }
