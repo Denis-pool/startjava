9313f330fa4914f6606b7a08144d1a10540754bb
Denis-pool <kriv.denkk@gmail.com>
2024-10-19 14:17:28 +0300

    Добавил новый файл с ДЗ по переменным

---
+++ VariableNamesTheme.java
@@ -0,0 +1,120 @@
+public class VariableNamesTheme {
+
+    public static void main(String[] args) {
+
+        System.out.println("\n1.Разные переменные");
+
+        // Цифра
+        int num = 52;
+        System.out.println("\nЦифра = " + num);
+
+        // Сумма чисел
+        int num2 = 23;
+        int sum = num + num2;
+        System.out.println("\nСумма чисел " + num + " и " + num2 + " = " + sum);
+        
+        // Произведение чисел
+        int result = num * num2;
+        System.out.println("\nПроизведение чисел " + num + " и " + num2 + " = " + result);
+
+        // Максимальное число 
+        int maxNum = 52;
+        System.out.println("\nМаксимальное число - " + maxNum);
+
+        // Количество десятков
+        int countTen = 5;
+        System.out.println("\nКоличество десятков = " + countTen);
+
+        // Вес собаки
+        int weightDog = 35;
+        System.out.println("\nВес собаки - "+ weightDog);
+
+        // Исходное число
+        int originalNum = 52;
+        System.out.println("\nИсходное число - " + originalNum);
+
+        // Процент по вкладу
+        int percentDep = 15;
+        System.out.println("\nПроцент по вкладу = " + percentDep);
+
+        // Переменная хранит символ &
+        char haveAmpersand = '&';
+        System.out.println("\nПеременная хранит символ " + haveAmpersand);
+
+        // Код ошибки
+        int errorCode = 502;
+        System.out.println("\nКод ошибки - " + errorCode);
+
+        // Тип сообщения 
+        int messageType = 102;
+        System.out.println("\nТип текущего сообщения - " + messageType);
+
+        // Число нулей
+        int countZero = 3;
+        System.out.println("\nЧисло нулей в числе 1000 = " + countZero);
+
+        // Уникальное число
+        int uniqueNum = 52;
+        System.out.println("\nУникальное число = " + uniqueNum);
+
+        // Случайное число
+        int randomNum = 97;
+        System.out.println("\nСлучайное число = " + randomNum);
+
+        // Математическое выражение
+        String mathFormula = " x + y = c";
+        System.out.println("\nМатематическое выражение " + mathFormula);
+
+        // Счет в игре
+        String gameScore = "6:2";
+        System.out.println("\nСчет в игре " + gameScore);
+
+        // Максимальная длина
+        int maxLenght = 340; 
+        System.out.println("\nМаксимальная длина " + maxLenght);
+
+        // Пункт меню
+        int menuPoint = 7;
+        System.out.println("\nПункт меню - " + menuPoint);
+
+        // Стоимость кофе
+        int coffeeCost = 320;
+        System.out.println("\nСтоимость кофе - " + coffeeCost);
+
+        // Дата начала
+        String dateStart = "25.11.2024";
+        System.out.println("\nДата начала " + dateStart);
+
+        // Окончание диапазона
+        int rangeEnd = 99;
+        System.out.println("\nОкончание диапазона " + rangeEnd);
+
+        // Полное имя работника месяца
+        String fullNameBestWorkerInMonth = "Walter Hartwell White";
+        System.out.println("\nПолное имя работника месяца " + fullNameBestWorkerInMonth);
+
+        // Заголовок электронной книги
+        String eBookTitle = "Never Give Up";
+        System.out.println("\nЗаголовок электронной книги " + eBookTitle);
+
+        // Размер
+        int size = 52;
+        System.out.println("\nРазмер одежды - " + size);
+
+        // Вместимость
+        int capacity = 11;
+        System.out.println("\nВместимость " + capacity);
+
+        // Счетчик
+        int counter = 52;
+        System.out.println("\nСчетчик = " + counter);
+
+        // Путь до файла
+        String filePath = "D:StartJava";
+        System.out.println("\nПуть до файла " + filePath);
+
+        // Количество чисел в каждой строке
+        int sumNumbersInEachRow = 51;
+        System.out.println("\nКоличество чисел в каждой строке " + sumNumbersInEachRow);
+    }
+}
