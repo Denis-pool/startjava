a248bf7cf373da3a49360856c30d22a7e71196ba
Denis-pool <kriv.denkk@gmail.com>
2024-11-01 13:30:33 +0300

    Сделал все кроме 5 и 6 задачи

--- IfElseStatementTheme.java
+++ IfElseStatementTheme.java
@@ -73,7 +73,7 @@ public class IfElseStatementTheme {
         System.out.println("\n4.Поиск одинаковых цифр в числах\n");
 
         var coupleNum1 = 123;
-        var coupleNum2 = 223;
+        var coupleNum2 = 123;
         var first1 = coupleNum1/100;
         var first2 = coupleNum2/100;
         var third1 = coupleNum1 % 10;
@@ -81,10 +81,95 @@ public class IfElseStatementTheme {
         var second1 = coupleNum1/10 % 10;
         var second2 = coupleNum2/10 % 10;
         
-        if (first1 != first2 || second1 != second2 || third1 != third2) {
-            System.out.println("Одинаковых чисел нет");
+        System.out.println("Исходные числа " + coupleNum1 + " и " + coupleNum2);
+        if (first1 != first2 && second1 != second2 && third1 != third2) {
+            System.out.println("Одинаковых цифр нет");
         }
-        else if 
- // Очень сложно. не могу придумать как оптимально это сделать. Не хочу перебирать все варианты и делать сложную структуру ветвления
+        else if (first1 == first2 && second1 != second2 && third1 != third2) {
+            System.out.println("Одна одинаковая цифра " + first1 + " в первом разряде");
+            }
+            else if (first1 != first2 && second1 == second2 && third1 != third2) {
+                System.out.println("Одна одинаковая цифра " + second1 + " во втором разряде");
+                }
+                else if (first1 != first2 && second1 != second2 && third1 == third2) {
+                    System.out.println("Одна одинаковая цифра " + third1 + " в третьем разряде");
+                    }
+                    else if (first1 == first2 && second1 != second2 && third1 == third2) {
+                        System.out.println("Две одинаковые цифры " + first1 + " и " + third1 + " в первом и третьем разряде");
+                        }
+                        else if (first1 == first2 && second1 == second2 && third1 != third2) {
+                            System.out.println("Две одинаковые цифры " + first1 + " и " + second1 + " в первом и втором разряде");
+                            }
+                            else if (first1 != first2 && second1 == second2 && third1 == third2) {
+                                System.out.println("Две одинаковые цифры " + second1 + " и " + third1 + " во втором и третьем разряде");
+                                }
+                                else {
+                                    System.out.println("Все три цифры одинаковые " + first1 + ", " + second1 + " и " + third1);
+                                }
+
+        // Сделал верхнюю задачу ооочень коряво и сложно. По другому не придумал как сделать. 
+
+        System.out.println("\n5.Определение символа по его коду");
+        // сложно 
+
+        System.out.println("\n6.Подсчет начисленных банком %");
+        var deposit = 321123.59;
+        // Не полное условие задачи
+
+        System.out.println("\n7.Определение оценки по предметам");
+        var historyPercent = 59;
+        var progrPercent = 92;
+        var averagePercent = (double)(progrPercent + historyPercent)/2;
+        int scoreHistory;
+        int scoreProgr;
+        double averageScore;
+        if (historyPercent > 91) {
+            System.out.println("История - 5");
+            scoreHistory = 5;
+            } else if (historyPercent <= 91 && historyPercent > 73) {
+                    System.out.println("История - 4");
+                    scoreHistory = 4;
+                    } else if (historyPercent <= 73 && historyPercent > 60) {
+                            System.out.println("История - 3");
+                            scoreHistory = 3;
+                            } else {
+                            System.out.println("История - 2");
+                            scoreHistory = 2;
+                            }
+        if (progrPercent > 91) {
+            System.out.println("Программирование - 5");
+            scoreProgr = 5;
+            } else if (progrPercent <= 91 && progrPercent > 73) {
+                    System.out.println("Программирование - 4");
+                    scoreProgr = 4;
+                    } else if (progrPercent <= 73 && progrPercent > 60) {
+                            System.out.println("Программирование - 3");
+                            scoreProgr = 3;
+                            } else {
+                                System.out.println("Программирование - 2");
+                                scoreProgr = 2;
+                            }
+        averageScore = (double)(scoreProgr + scoreHistory) / 2;
+        System.out.println("Средний бал - " + averageScore);
+        System.out.println("Средний % по предметам " + averagePercent);
+
+        /* Если бы средний бал считался относительно среднего процента, то можно было бы реализовать так: 
+        if (averagePercent > 91) {
+            System.out.println("Средний бал - 5");
+            } else if (averagePercent <= 91 && averagePercent > 73) {
+                    System.out.println("Средний бал - 4");
+                    } else if (averagePercent <= 73 && averagePercent > 60) {
+                            System.out.println("Средний бал - 3");
+                            } else System.out.println("Средний бал - 2"); */
+
+        System.out.println("\n8.Расчет годовой прибыли");
+        var monthSale = 13025.233;
+        var rentOfficeMonth = 5123.018;
+        var devCostMonth = 9001.729;
+        var yearProfit = (double)(monthSale - devCostMonth - rentOfficeMonth) * 12;
+        if (yearProfit > 0) {
+            System.out.println("Прибыль за год: +" + String.format("%.3f", yearProfit) + " руб");
+        } else System.out.println("Прибыль за год: " + String.format("%.3f", yearProfit) + " руб");
+        
     }
 }
