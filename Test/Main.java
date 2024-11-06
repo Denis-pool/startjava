10a524f652f9a94e9d45dd7cc8b44a8bff57842f
Denis-pool <kriv.denkk@gmail.com>
2024-11-06 22:44:12 +0300

    +

--- Test/Main.java
+++ Test/Main.java
@@ -4,14 +4,28 @@ public class Main {
 
         // Просуммировать числа от 1 до 5
 
-      //  int sum = 1 + 2;
-      //  int sum1 = sum + 3;
-      //  int sum2 = sum1 + 4;
-      // int result = sum2 + 5;
-      //  System.out.println(result);
+      // //  int sum = 1 + 2;
+      // //  int sum1 = sum + 3;
+      // //  int sum2 = sum1 + 4;
+      // // int result = sum2 + 5;
+      // //  System.out.println(result);
 
 
-        System.out.print("Написано однажды, ");
-        System.out.println("работает везде!");
+      //   System.out.print("Написано однажды, ");
+      //   System.out.println("работает везде!");
+
+
+        char  variable = '\u0057';
+        if (Character.isLetter(variable)) {
+            if (Character.toUpperCase(variable) == variable) {
+                System.out.println(variable + " - большая буква");
+            } else {
+                System.out.println(variable + " - маленькая буква");
+                }
+        } else if (Character.isDigit(variable)) {
+            System.out.println(variable + " - цифра");
+                } else {
+                    System.out.println(variable + " - ни буква и ни цифра");
+        }
     }
 }
